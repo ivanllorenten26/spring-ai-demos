67143dd8176ef5f86fc821ea6f613822c0e0126d
ivanllorenten26 <ivan.llorente@n26.com>
2025-04-06 22:19:42 +0200

    initial commit

---
+++ src/main/kotlin/com/ivanllorente/springaidemo/SpringAiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.ivanllorente.springaidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ivanllorente/springaidemo/SpringAiDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ivanllorente.springaidemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
