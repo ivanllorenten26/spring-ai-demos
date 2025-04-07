c6299c24d90529ad909895f546eb92e427b3b014
ivanllorenten26 <ivan.llorente@n26.com>
2025-04-07 23:13:33 +0200

    add model configuration

--- src/main/kotlin/com/ivanllorente/springaidemo/infra/ai/OpenAiChatService.kt
+++ src/main/kotlin/com/ivanllorente/springaidemo/infra/ai/OpenAiChatService.kt
@@ -2,6 +2,8 @@ package com.ivanllorente.springaidemo.infra.ai
 
 import com.ivanllorente.springaidemo.domain.interfaces.AiChatService
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.openai.OpenAiChatOptions
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,7 +14,23 @@ class OpenAiChatService(
 
     override fun getResponse(question: String): String? {
 
-        val response = chatClient.prompt(question)
+        //val response = chatClient.prompt(question)
+        val options = OpenAiChatOptions()
+        options.model = "gpt-3.5-turbo"
+        options.temperature = 0.7
+        options.maxTokens = 1000
+        options.topP = 1.0
+        options.frequencyPenalty = 0.0
+        options.presencePenalty = 0.0
+        options.stop = listOf("\n", "Human:", "AI:")
+        options.n = 1
+
+        val response = chatClient.prompt(
+            Prompt(
+                question,
+                options
+            )
+        )
         return response.call().chatResponse()?.results?.first()?.output?.text
     }
 }
